e02121f60e2b277823f7773a63b5737b63f84b31
luke gartland <lukegartland@lukes-MacBook-Pro-2.local>
2022-12-21 08:54:13 -0600

    bad code, limits other types

--- build/index.js
+++ build/index.js
@@ -1,1 +1,31 @@
-console.log('hi');
+"use strict";
+class Sorter {
+    constructor(collection) {
+        this.collection = collection;
+    }
+    sort() {
+        const { length } = this.collection;
+        // only works if collection is array of numbers
+        // bubble sort
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+                //type guard
+                if (this.collection instanceof Array) {
+                    // if collection is array of #'s
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        const leftHand = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = leftHand;
+                    }
+                }
+                if (typpof)
+                    this.collection === 'string';
+                {
+                }
+            }
+        }
+    }
+}
+const sorter = new Sorter([10, 8, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
